43801360c2a0040c837a859d289be1443c9c661d
saj panchal <sajpanchal2020@outlook.com>
2021-09-17 13:46:16 -0400

    code to caculate the s-expression added. modified main regex string.

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,21 +9,92 @@ import SwiftUI
 
 struct ContentView: View {
     @State var myString = ""
-   @State var regex = try! NSRegularExpression(pattern:#"[(](add|multiply)\s([0-9]+|[(](add|multiply)\s([0-9]+)\s[0-9]+[)])\s([0-9]+|[(](add|multiply)\s([0-9]+)\s[0-9]+[)])[)]"#)
-    
+    @State var subString = ""
+   // @State var regex = try! NSRegularExpression(pattern:#"[(](add|multiply)\s([0-9]+|[(](add|multiply)\s([0-9]+)\s[0-9]+[)])\s([0-9]+|[(](add|multiply)\s([0-9]+)\s[0-9]+[)])[)]"#)
+    @State var regex = try! NSRegularExpression(pattern: #"([(](add|multiply)\s((([0-9]|((\?1))))\s([0-9]|((\?1)))[)]))"#)
+    @State var subRegex = try! NSRegularExpression(pattern:#"[(](add|multiply)\s([0-9]+)\s[0-9]+[)]"#)
+    @State var flag = true
     @State var result = false
+    @State var answer = 0
     var body: some View {
-        VStack {
-            TextField("Enter function", text: $myString)
-                .padding()
-            Button("check") {
-               result = regex.matches(myString)
-                print(result)
+        NavigationView {
+            VStack {
+                TextField("Enter function", text: $myString)
+                    .padding()
+                    .multilineTextAlignment(.center)
+    
+                Spacer()
+                Button("check") {
+                    answer = 0
+                    subString = myString
+                    flag = true
+                    result = regex.matches(myString)
+                    print(result)
+                    if result {
+                        
+                       while flag {
+                            let matchedString = findSubString()
+                           let matchedArr = matchedString.components(separatedBy: CharacterSet(charactersIn: " ()"))
+                            if matchedArr[1] == "add" {
+                               let result = add(exp1: matchedArr[2], exp2: matchedArr[3])
+                                print(subString, matchedString, result)
+                                subString = subString.replacingOccurrences(of: matchedString, with: result)
+                               // print(subString)
+                                
+                            }
+                            else if matchedArr[1] == "multiply" {
+                                let result = multiply(exp1: matchedArr[2], exp2: matchedArr[3])
+                                 print(subString, matchedString, result)
+                                 subString = subString.replacingOccurrences(of: matchedString, with: result)
+                               // print(subString, subString.count)
+                            }
+                        if subString.contains("add") || subString.contains("multiply") {
+                            flag = true
+                            //print("FLAG:",flag)
+                        }
+                        else {
+                            flag = false
+                            //print("FLAG:",flag)
+                            answer = Int(subString)!
+                            break
+                        }
+                            
+                       }
+                                
+                    }
+                }
+                .frame(width: 150, height: 50, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                .background(Color.blue)
+                .foregroundColor(Color.white)
+                .cornerRadius(16)
+                Spacer()
+                Text(result ? "Calculation: \(answer)" : "incorrect function syntax.")
             }
-            Text(result ? "pass": "fail")
         }
-        
+      
     }
+    
+    func findSubString() -> String {
+        let matches = subRegex.matches(in: subString, options: [], range: NSRange(location: 0, length: subString.utf16.count))
+        if let match = matches.first {
+            let range = match.range(at: 0)
+            if let swiftRange = Range(range, in: subString) {
+                let result = subString[swiftRange]
+                return String(result)
+            }
+        }
+        return ""
+    }
+    
+    func add(exp1:String, exp2: String) -> String {
+        let result = Int(exp1)! + Int(exp2)!
+        return String(result)
+    }
+    func multiply(exp1:String, exp2: String) -> String {
+        let result = Int(exp1)! * Int(exp2)!
+        return String(result)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -37,4 +108,5 @@ extension NSRegularExpression {
         let range = NSRange(location:0, length: string.utf16.count)
         return firstMatch(in: string, options:[], range:range) != nil
     }
+    
 }
